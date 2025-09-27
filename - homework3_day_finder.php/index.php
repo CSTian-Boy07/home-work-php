9bfd700906f0b547423caa3ea07a3721d415f43e
Asif Ahmed Abir <cstianboy07@gmail.com>
2025-09-27 11:50:26 +0600

    day_finder_php

---
+++ - homework3_day_finder.php/index.php
@@ -0,0 +1,32 @@
+<?php 
+
+//day finder
+
+$getDayNum=2;
+switch($getDayNum)
+{
+    case 1;
+    echo "Day is: sunday";
+    break;
+    case 2;
+    echo "Day is: monday";
+    break;
+    case 3;
+    echo "Day is: tueday";
+    break;
+    case 4;
+    echo "Day is: wedday";
+    break;
+    case 5;
+    echo "Day is: thuday";
+    break;
+    case 6;
+    echo "Day is: friday";
+    break;
+    case 7;
+    echo "Day is: satday";
+    break;
+    default:
+    echo "Day is: invalid";
+}
+?>
